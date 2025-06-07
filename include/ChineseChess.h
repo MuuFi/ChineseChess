e030504278a0209bb1899f83c38e1c7c1d1d81cf
MuuFi <YourMuuFi@outlook.com>
2025-06-07 22:56:30 +0800

    Add struction.

--- include/ChineseChess.h
+++ include/ChineseChess.h
@@ -7,27 +7,37 @@
 #include <fstream>
 #include <utility>
 
+using Pos = std::pair<int, int>;
+using VecXQ = std::vector<XiangqiPiece>;
+using String = std::string;
+
 struct XiangqiPiece {
-    char type;
-    std::pair<int, int> Position;
+    char type;            // 区分棋子类型
+    Pos Position;         // 记录棋子位置
     XiangqiPiece(char ch);
+    XiangqiPiece(char ch, const Pos& p);
 };
 class Player {
 private:
-    std::string name;
-    std::vector<XiangqiPiece> retainPieces;
-public:
-    Player();
-    Player(std::string);
-    Player(std::string, const std::vector<XiangqiPiece>& initialize);
-    Player(std::ifstream& initFile);
+    String name;       // 用户名
+    VecXQ retainPieces;// 用户保有的棋子
 
-    void serialize();
+public:
+    Player();                                        // 默认构造
+    Player(String str);                              // 输入用户名
+    Player(String str, const VecXQ& initialize);     // 输入用户名和用户保有的棋子构造
+    Player(std::ifstream& initFile);                 // 调用 deserialize() 从文件读取
+
+    void serialize(std::ofstream& sf);               // 把类数据序列化到文件
+    void deserialize(std::ifstream& lf);             // 从文件读取序列化到数据
+    std::vector<Pos> predicted(size_t PieceIdx);     // 根据索引从 VecXQ 中读取对应的棋子然后根据棋子的 type 预测他的落点 
+    void move(size_t PieceIdx, const Pos& targetPos);// 把索引对应的棋子移动到目标位置(targetPos)
 };
 class XiangqiGame {
 private:
-    Player gamePlayer[2];
+    Player gamePlayer[2];// 用户
+    int board[9][10];    // 棋盘
 public:
-
+    XiangqiGame();
 };
 #endif
--- src/ChineseChess.cpp
+++ src/ChineseChess.cpp
@@ -1,3 +1,19 @@
 #include "ChineseChess.h"
 
+using Pos = std::pair<int, int>;
+using VecXQ = std::vector<XiangqiPiece>;
+using String = std::string;
+
 XiangqiPiece::XiangqiPiece(char ch = 0): type(ch), Position(0, 0) {}
+XiangqiPiece::XiangqiPiece(char ch, const Pos& p): type(ch), Position(p) {}
+
+Player::Player(): name(), retainPieces() {}
+Player::Player(String str): name(str), retainPieces() {}
+Player::Player(String str, const VecXQ& initialize): name(str), retainPieces(initialize) {}
+Player::Player(std::ifstream& initFile) { deserialize(initFile); }
+
+// TODO: 一堆函数没写
+void Player::serialize(std::ofstream& sf) {}
+void Player::deserialize(std::ifstream& lf) {}
+std::vector<Pos> Player::predicted(size_t PieceIdx) {}
+void Player::move(size_t PieceIdx, const Pos& targetPos) {}
